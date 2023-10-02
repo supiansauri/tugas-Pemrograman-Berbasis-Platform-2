7132c7dc6359e09a9875e01ee8d5b8fb31adcc2b
supiansauri <114654547+supiansauri@users.noreply.github.com>
2023-10-02 21:10:09 +0700

    Add files via upload

---
+++ tugas-2.js
@@ -0,0 +1,9 @@
+let firstName = "Supian";  
+let lastName = "Sauri";     
+let age = 20;              
+let isMarried = false;     
+
+console.log("Nama Depan        :", firstName);
+console.log("Nama Belakang     :", lastName);
+console.log("Umur              :", age);
+console.log("Status Pernikahan :", (isMarried ? "Menikah" : "Belum Menikah"));
